d6e9b5ea9b13c0b1b16fcb328421ca8fcfb4a25b
Xiaoqing Liu <christinalxq222@gmail.com>
2014-09-30 19:26:37 +0100

    Initial project structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mytwitter::Application.config.secret_key_base = 'cd51c19e9336bfac68d625fd82107d40d17318b4f8f8f168a935eb9a1834b1bfa046a8ae967d676dce6ecb69f22a9df53ff79e124a97aecca0d23c8f44f8a2c9'
